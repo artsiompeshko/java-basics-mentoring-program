e700d1c2bedc009273829d507fbbe174dbe4dffa
Artsiom <peshkoartembsu@gmail.com>
2020-11-22 14:08:19 +0300

    add calculator implementation

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,23 @@
+import java.lang.Math;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public int sqrt(int a) {
+        return Math.sqrt(a);
     }
 }
