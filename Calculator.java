25b6a625f5fe864f39c6362fa3f9c28b63831ce4
Artsiom <peshkoartembsu@gmail.com>
2020-11-22 14:32:25 +0300

    add with 3 params

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,13 @@ import java.lang.Math;
 
 public class Calculator {
     public int add(int a, int b) {
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
         return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        System.out.println("This is add method for 3 params");
+        return this.add(a, b) + c;
     }
 
     public int sub(int a, int b) {
